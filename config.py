005505365d810c81426976c06dc5ad069e208a7c
Jules Fredes C. <julfredes@ug.uchile.cl>
2025-10-08 22:25:24 -0300

    Correccion de detalles

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    SECRET_KEY = 'tu-clave-secreta-aqui'
+    SECRET_KEY = 'pr0gr4m4c10nw3b'
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://cc5002:programacionweb@localhost:3306/tarea2'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = 'static/uploads'
